85cb9b4945d35606cc6e59a1a7def6e668e7b1ca
Han Kim <hanniek00@gmail.com>
2022-02-24 21:37:39 -0600

    Added starter project

---
+++ Starter/Werdd/Werdd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Werdd
+//
+//  Created by Han Kim on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
