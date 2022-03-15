9249fd1e052b875df5d7aa6c2093d2f072cf58d6
Han Kim <hanniek00@gmail.com>
2022-03-15 12:25:05 -0500

    ViewController - update auto layout constraints

--- Complete/Werdd/Werdd/HomeViewController.swift
+++ Complete/Werdd/Werdd/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class ViewController: UIViewController {
 
     // MARK: - Properties
     
@@ -39,7 +39,7 @@ class HomeViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "noun"
-        label.font = UIFont(name: "Helvetica-Oblique", size: 14)
+        label.font = UIFont(name: "Rubik-Italic", size: 14)
         return label
     }()
     
@@ -47,9 +47,9 @@ class HomeViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "creating a sequence of instructions to enable the computer to do something"
-        label.font = UIFont(name: "Rubik-Light", size: 12)
-        label.numberOfLines = 0
+        label.font = UIFont(name: "Rubik-Regular", size: 12)
         label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
         return label
     }()
     
@@ -58,21 +58,23 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(named: "Taupe")
+        view.backgroundColor = UIColor(named: "TaupeColor")
         
         addSubviews()
+        addConstraints()
     }
-
+    
     // MARK: - UI Setup
     
     func addSubviews() {
-        
         view.addSubview(appTitleLabel)
         view.addSubview(blueView)
         blueView.addSubview(wordTitleLabel)
         blueView.addSubview(partsOfSpeechLabel)
         blueView.addSubview(wordDefinitionLabel)
-        
+    }
+    
+    func addConstraints() {
         NSLayoutConstraint.activate([
             appTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             appTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -85,15 +87,14 @@ class HomeViewController: UIViewController {
             
             wordTitleLabel.topAnchor.constraint(equalTo: blueView.topAnchor, constant: 20),
             wordTitleLabel.leadingAnchor.constraint(equalTo: blueView.leadingAnchor, constant: 20),
-            wordTitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: partsOfSpeechLabel.leadingAnchor),
             
             partsOfSpeechLabel.topAnchor.constraint(equalTo: wordTitleLabel.topAnchor, constant: 8),
             partsOfSpeechLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.trailingAnchor, constant: 5),
-            partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: blueView.trailingAnchor, constant: -20),
+            partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: blueView.trailingAnchor),
             
             wordDefinitionLabel.topAnchor.constraint(equalTo: partsOfSpeechLabel.bottomAnchor, constant: 20),
             wordDefinitionLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.leadingAnchor),
-            wordDefinitionLabel.trailingAnchor.constraint(lessThanOrEqualTo: blueView.trailingAnchor, constant: -20)
+            wordDefinitionLabel.trailingAnchor.constraint(equalTo: blueView.trailingAnchor, constant: -20)
         ])
     }
 }
