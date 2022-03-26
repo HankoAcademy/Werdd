2f557e052b65d6cf55f7860e642ba6c01ef7784a
Han Kim <hanniek00@gmail.com>
2022-03-26 13:57:14 -0500

    Create starter class for custom cell WorldTableViewCell

---
+++ Complete/Werdd/Werdd/WordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WordTableViewCell.swift
+//  Werdd
+//
+//  Created by Hannie Kim on 3/20/22.
+//
+
+import UIKit
+
+class WordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
