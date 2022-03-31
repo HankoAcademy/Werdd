be50141e5b9af44397e278ab9428c2cd63df36da
Han Kim <hanniek00@gmail.com>
2022-03-30 22:15:17 -0500

    Create subclass for random word view

--- Complete/Werdd/Werdd/RoundedViewWithColor.swift
+++ Complete/Werdd/Werdd/RoundedViewWithColor.swift
@@ -10,17 +10,78 @@ import UIKit
 class RoundedViewWithColor: UIView {
     
     let color: UIColor?
+    var completion: (() -> Void)?
     
-    init(color: UIColor?, frame: CGRect = .zero) {
+    let wordTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Rubik-Bold", size: 24)
+        return label
+    }()
+    
+    let partsOfSpeechLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Helvetica-Oblique", size: 14)
+        return label
+    }()
+    
+    let wordDefinitionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Rubik-Light", size: 12)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    lazy var refreshButton: RefreshButton = {
+        let button = RefreshButton { [weak self] in
+            self?.completion?()
+        }
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    init(color: UIColor?, frame: CGRect = .zero, completion: (() -> Void)?) {
+        self.completion = completion
         self.color = color
         
         super.init(frame: frame)
         
         backgroundColor = color
         layer.cornerRadius = 20
+        
+        setUpSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpSubviews() {
+        
+        addSubview(wordTitleLabel)
+        addSubview(partsOfSpeechLabel)
+        addSubview(wordDefinitionLabel)
+        addSubview(refreshButton)
+        
+        NSLayoutConstraint.activate([
+            wordTitleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            wordTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            wordTitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: partsOfSpeechLabel.leadingAnchor),
+            
+            partsOfSpeechLabel.topAnchor.constraint(equalTo: wordTitleLabel.topAnchor, constant: 8),
+            partsOfSpeechLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.trailingAnchor, constant: 5),
+            partsOfSpeechLabel.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -20),
+            
+            wordDefinitionLabel.topAnchor.constraint(equalTo: partsOfSpeechLabel.bottomAnchor, constant: 20),
+            wordDefinitionLabel.leadingAnchor.constraint(equalTo: wordTitleLabel.leadingAnchor),
+            wordDefinitionLabel.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -20),
+            
+            refreshButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            refreshButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            refreshButton.widthAnchor.constraint(equalTo: refreshButton.heightAnchor)
+        ])
     }
 }
