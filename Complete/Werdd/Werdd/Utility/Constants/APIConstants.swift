f65679704f0f4d387b52e94c806054241e25776b
Han Kim <hanniek00@gmail.com>
2023-05-29 23:01:19 -0500

    Reorganize files and folders

---
+++ Complete/Werdd/Werdd/Utility/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  Werdd
+//
+//  Created by Hannie Kim on 5/14/23.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let apiKey = "b3141599f8mshded1c1316f28eebp1fb919jsn4885af8ae6f7"
+}
